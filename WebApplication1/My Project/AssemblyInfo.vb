7cce8d38cb235dfe2b7adcea51fb065f81672596
samerMC <smonge771@gmail.com>
2026-06-19 18:22:03 -0600

    Initial commit

---
+++ WebApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("WebApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication1")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("0815d5e9-fd4d-4a67-9775-53038534f9e7")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
